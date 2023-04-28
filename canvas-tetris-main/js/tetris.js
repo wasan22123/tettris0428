2485bf28840a8c0a135a4a1b2a129ed238e7f237
Nisuimura Wakana <221205123@ccmailg.meijo-u.ac.jp>
2023-04-28 14:00:10 +0900

    greenを少しかわいくした

--- canvas-tetris-main/js/tetris.js
+++ canvas-tetris-main/js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'cyan', 'orange', 'blue', 'yellow', 'red', 'green', 'purple'
+    'cyan', 'orange', 'blue', 'yellow', 'red', 'chartreuse', 'purple'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
